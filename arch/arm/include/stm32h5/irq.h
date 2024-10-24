20837267e16eaeab8900eef6d2dbfcc7a0aebea9
Kyle Wilson <kwilson@2g-eng.com>
2024-10-24 12:29:24 -0500

    Fixed selection of irq file.

--- arch/arm/include/stm32h5/irq.h
+++ arch/arm/include/stm32h5/irq.h
@@ -31,9 +31,9 @@
 
 #include <nuttx/config.h>
 
-#if defined(CONFIG_STM32H5_STM32H56XXX) || defined(CONFIG_STM32H5_STM32H57XXX)
+#if defined(CONFIG_STM32H5_STM32H52XXX) || defined(CONFIG_STM32H5_STM32H53XXX)
 #  include <arch/stm32h5/stm32h52xxx_3xxx_irq.h>
-#elif defined(CONFIG_STM32H5_STM32H52XXX) || defined(CONFIG_STM32H5_STM32H53XXX)
+#elif defined(CONFIG_STM32H5_STM32H56XXX) || defined(CONFIG_STM32H5_STM32H57XXX)
 #  include <arch/stm32h5/stm32h56xxx_7xxx_irq.h>
 #else
 #  error "Unsupported STM32 H5 chip"
